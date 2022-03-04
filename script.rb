89b860cc86e5ca2bf69d5135c95f9dfd7525e4e5
Andy Park <apark1214@gmail.com>
2022-03-03 18:04:23 -0800

    working on rest of the questions

--- script.rb
+++ script.rb
@@ -1,3 +1,19 @@
 =begin
 This is going to be a re run on what I learned on basic syntax and couple string methods for Ruby
+This exercise will ask for the users information
 =end
+
+
+print "What is your first name: "
+my_name = gets.chomp
+
+print "What is your last name: "
+last_name =gets.chomp
+
+print "which city are you from: "
+city = gets.chomp
+
+print "which state are you from:"
+state= gets.chomp
+
+print "Your name is #{my_name}"
