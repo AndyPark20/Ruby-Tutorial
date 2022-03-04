1367e41397f03e926685d8f0a80bb63c3951f691
Andy Park <apark1214@gmail.com>
2022-03-03 18:10:39 -0800

    working on loops

--- script.rb
+++ script.rb
@@ -4,16 +4,16 @@ This exercise will ask for the users information
 =end
 
 
-print "What is your first name: "
-my_name = gets.chomp
+# print "What is your first name: "
+# my_name = gets.chomp.capitalize!
 
-print "What is your last name: "
-last_name =gets.chomp
+# print "What is your last name: "
+# last_name =gets.chomp.capitalize!
 
-print "which city are you from: "
-city = gets.chomp
+# print "which city are you from: "
+# city = gets.chomp.capitalize!
 
-print "which state are you from:"
-state= gets.chomp
+# print "which state are you from:"
+# state= gets.chomp.capitalize!
 
-print "Your full name is #{my_name} #{last_name} and currently lives in #{city}, #{state}"
+# print "Your full name is #{my_name} #{last_name} and currently lives in #{city}, #{state} "
