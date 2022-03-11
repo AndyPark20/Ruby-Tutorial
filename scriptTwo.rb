1833bf467edc36c798b9b31d4fe3a828464936ee
AndyPark20 <apark1214@gmail.com>
2022-03-11 09:42:19 -0800

    working with hash

--- scriptTwo.rb
+++ scriptTwo.rb
@@ -30,11 +30,35 @@
 # printHello('andy')
 
 #Function to sort
-def sort_it(array)
-    array.sort! do |x,y|
-        y <=>x
-    end
-    print array
-end
+# def sort_it(array)
+#     array.sort! do |x,y|
+#         y <=>x
+#     end
+#     print array
+# end
 
-sort_it([7,10,1,5,6])
+# sort_it([7,10,1,5,6])
+
+# using Objects
+movie_ratings = {
+  memento: 3,
+  primer: 3.5,
+  the_matrix: 5,
+  truman_show: 4,
+  red_dawn: 1.5,
+  skyfall: 4,
+  alex_cross: 2,
+  uhf: 1,
+  lion_king: 3.5
+}
+
+=begin
+movie_ratings.each_key do |x|
+    puts "title of the movie is #{x}"
+end
+=end
+
+#Print values
+move_ratings.each_key do |x|
+    puts "Rating for the movie is #{x}"
+end
