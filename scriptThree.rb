1ba11594d39bd37ccf64274272de15d2a44ad9ae
AndyPark20 <apark1214@gmail.com>
2022-03-16 14:20:13 -0700

    completed with sort

--- scriptThree.rb
+++ scriptThree.rb
@@ -26,14 +26,40 @@
 
 #define a function and depending on the input number output a value
 
-puts "How would you like to be greeted? "
-answer = gets.chomp
+# puts "How would you like to be greeted? "
+# answer = gets.chomp
 
-def greetings(value)
-    case value
-    when "hello" then puts "Hello there!"
-    when "goodbye" then puts "goodbye to you as well!"
-    end
+# def greetings(value)
+#     case value
+#     when "hello" then puts "Hello there!"
+#     when "goodbye" then puts "goodbye to you as well!"
+#     end
+# end
+
+# greetings(answer)
+
+
+#square root the number and check if the value is equal to 4, return all tested logic
+
+# array= [5,4,6,2,1]
+
+# def check_number(number)
+#     mapped =number.map do |x|
+#         if x * x == 4
+#          x
+#         end
+#     end
+#     print mapped
+# end
+
+
+# check_number(array)
+
+array =[5,4,6,10]
+
+array.sort! do |x,y|
+    y <=> x
 end
 
-greetings(answer)
+puts array
+
