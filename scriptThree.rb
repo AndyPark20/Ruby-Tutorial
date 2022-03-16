63ab54e09a16411ed55ce2ab2d00a812481b2858
AndyPark20 <apark1214@gmail.com>
2022-03-16 14:04:46 -0700

    case switches completed

--- scriptThree.rb
+++ scriptThree.rb
@@ -8,17 +8,32 @@
 
 #Change the strings into symbol
 
-andy ={
-    age: 26,
-    profession: "engineer",
-    food: "pizza"
+# andy ={
+#     age: 26,
+#     profession: "engineer",
+#     food: "pizza"
 
-}
+# }
 
-#add key/values
-andy[:sport] = "f1 racing"
+# #add key/values
+# andy[:sport] = "f1 racing"
 
-#delete key/values
-andy.delete(:age)
+# #delete key/values
+# andy.delete(:age)
 
-puts andy
+# puts andy
+
+
+#define a function and depending on the input number output a value
+
+puts "How would you like to be greeted? "
+answer = gets.chomp
+
+def greetings(value)
+    case value
+    when "hello" then puts "Hello there!"
+    when "goodbye" then puts "goodbye to you as well!"
+    end
+end
+
+greetings(answer)
