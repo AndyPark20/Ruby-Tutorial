8d8f4c3ae1cbc469570919548d8d38f6d4b97f04
AndyPark20 <apark1214@gmail.com>
2022-03-22 15:35:34 -0700

    save js custom code

--- indexTwo.js
+++ indexTwo.js
@@ -1,8 +1,7 @@
 // params to be stored client side as part of the SSA migration
 // this list must be kept in sync with any use of readInvocaData
-console.log('hello')
 try {
-    Invoca.Tools.allowedClientSideParams(['gcm_uid', 'gclid', 'msclkid','fbclid','gclsrc','gbraid','wbraid','utm_source','utm_medium']);
+    Invoca.Tools.allowedClientSideParams(['gcm_uid', 'gclid', 'msclkid','fbclid','gclsrc','gbraid','wbraid','utm_source', 'utm_medium']);
   } catch (error) {
     console.log(error);
   }
@@ -27,41 +26,27 @@ try {
   var gbraidPresent = Invoca.Tools.readUrl('gbraid') || Invoca.Tools.readInvocaData('gbraid');
   var wbraidPresent = Invoca.Tools.readUrl('wbraid') || Invoca.Tools.readInvocaData('wbraid');
   var utm_sourcePresent = Invoca.Tools.readUrl('utm_source') || Invoca.Tools.readInvocaData('utm_source');
-  var utm_mediumPresent = Invoca.Tools.readUrl('utm_medium') || Invoca.Tools.readInvocaData('utm_medium');
-  var sourceValues = ["zillow","Zillow"];
-  var utmMediumSourceValues =["organic", "Organic"];
-  
-
+  var utm_mediumePresent = Invoca.Tools.readUrl('utm_medium') || Invoca.Tools.readInvocaData('utm_medium');
+  var sourceValues = ["zillow","Zillow", "organic", "Organic"];
   
   // This is the the checking function for required values to swap, currently only one value, additional values should be added to the sourceValues Array
   function checkValuesToSwapOn(value) {
-    return value === utm_sourcePresent;
-  }
-  
-  //Function to check if the value from utm_mediumPresent matches any value stored in the utmMediumSourceValues array
-  function checkUtmMediumValues(source){
-    console.log('source',source);
-    console.log('utm_mediumPresent',utm_mediumPresent);
-    return source === utm_mediumPresent;
+    return value === utm_sourcePresent || value === utm_mediumePresent;
+    // return value === utm_mediumePresent;
   }
   
   // Moved INVSRC logic for running into this function, as customer wants to swap at all times for previous parameters, but only when utm_source is zillow.  The required param in UI does not allow for this additional requirement.  
-  function invocaShouldSwap() {
+  Invoca.Client.shouldSwap = function () {
     var swapUtm_source = sourceValues.some(checkValuesToSwapOn);
-   
-    //Check if the returned value from checkUtmMediumValues is present in utmMediumSourceValues
-    var swapUtm_medium = utmMediumSourceValues.some(checkUtmMediumValues);
     
-    if (gclidPresent || msclkidPresent || fbclidPresent || gclsrcPresent || gbraidPresent || wbraidPresent || swapUtm_source || swapUtm_medium){
-   
+    if (gclidPresent || msclkidPresent || fbclidPresent || gclsrcPresent || gbraidPresent || wbraidPresent || swapUtm_source){
       return true;
     } else {
-      
     return false;
     }
   }
   
   // Client leverages replace any number, so limiting autoSwap rather than autoRun to evaluate for required params
-  options.autoSwap = invocaShouldSwap();
+  options.autoRun = Invoca.Client.shouldSwap();
   
   return options;
