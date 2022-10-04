8ab90ce0ce19c52c6c90dc589f0d981f667b9f0c
AndyPark20 <apark1214@gmail.com>
2022-10-04 09:14:10 -0700

    working on else if

---
+++ control_flow_ruby.rb
@@ -0,0 +1,15 @@
+#If else statement
+#if you use if statement always end it with an end statement
+
+
+if 10<5
+    print "this is true"
+elsif 10==5
+    print "this is NOT TRUE"
+end
+
+
+
+#questions
+##javascript is else if and ruby is elsif
+
