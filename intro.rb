d41465130da825b8f14ef04bf01445e0ef05f077
AndyPark20 <apark1214@gmail.com>
2022-10-04 08:25:53 -0700

    learning about variables and math

--- intro.rb
+++ intro.rb
@@ -2,6 +2,17 @@
 Set the following variables to the corresponding values
 =end
 
+#variables
+#variables can be named with underscore
+
 my_num = 25;
 my_boolean true;
 my_string = "Hello";
+
+
+
+#learning about Math 
+#exponotential is with **
+#modulo is with %
+
+initial_times
