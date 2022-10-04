b548e82a06a72b5e11b978c2baf454324033d542
AndyPark20 <apark1214@gmail.com>
2022-10-04 16:58:24 -0700

    studying todays lesson material

--- methods/methods.rb
+++ methods/methods.rb
@@ -14,3 +14,6 @@ gsub! = replace a value with a new value
 #Gatcha!
 #unless is used to reverse if and else if statement
 #use "end" when using else and else if
+# ! in method are dangerous methods, meaning that it will mutate the original value if
+# IF ! is not in an method, this means that it is safe method, it will not change the original value
+# ?in a method is trying to say that it will show true or false value.
---
+++ test.rb
@@ -0,0 +1,24 @@
+# my_name = "andy"
+# revised_my_name =my_name.upcase
+
+# puts "original #{my_name} vs. revised #{revised_my_name}"
+
+
+
+=begin
+print vs puts   
+=end
+
+
+# print "What is your name?"
+# user_input = gets
+# print "What is your age?"
+# user_input_age = gets
+
+
+user_name = "andrew"
+user_name.upcase!
+
+print  user_name
+
+
