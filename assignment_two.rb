fd03dc1000a1d05c862b6cf7da934a3a71ac29fd
AndyPark20 <apark1214@gmail.com>
2022-10-04 08:56:02 -0700

    working on assignment 2

---
+++ assignment_two.rb
@@ -0,0 +1,13 @@
+#prompt user commands
+
+print "What's your first name?"
+
+
+
+=begin
+Important!!!
+gets is a Ruby method that gets response from the user
+chomp removes the line
+=end
+
+user_response =gets.chomp
