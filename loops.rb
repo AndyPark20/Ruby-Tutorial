310fa41233d648680f0bae752152e2f23c106998
AndyPark20 <apark1214@gmail.com>
2022-10-04 22:22:58 -0700

    working on loops

---
+++ loops.rb
@@ -0,0 +1,10 @@
+
+
+num =0
+
+loop do
+    num+=1
+    next if num % 2 ==1
+    puts num
+    break if num ==20
+end
--- methods/methods.rb
+++ methods/methods.rb
@@ -5,9 +5,18 @@ upcase = capitalizes everything
 downcase = lowercases everything
 gets = gets response from the user
 capitalize= capitalizes the first letter
-capitalize! = capitalizes the value within the variable;
 include? = looks for a specific value and if found within the value it will return true else it will return false;
 gsub! = replace a value with a new value
+
+
+#loops
+while loop = will loop thru as long as the condition is true
+Until loop = will loop thru as long as the condition is false
+For loop = will loop as long as the condition is true (.. will go to the highest value, ... will stop the second highest value)
+#Iterator loops
+loop do = ????
+#Iterator loops for Array
+Each
 
 
 
@@ -18,3 +27,13 @@ gsub! = replace a value with a new value
 # IF ! is not in an method, this means that it is safe method, it will not change the original value
 # ?in a method is trying to say that it will show true or false value.
 # gets also adds a blank line just like puts
+###LOOPS AND ITERATION#####
+#For loop uses 2 or 3 dots, 2 dots mean go to the highest number, 3 dots mean go to the number right before highest number
+#Unlike Javascript incrementing or decrementing only works with += -= rather than ++ or --
+#when to use loop do vs for loop
+
+
+
+
+#Need to go over
+#While vs until
