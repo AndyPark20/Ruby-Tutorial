8d1604df1a34ee879dad3ee0bdd76585708eb6ff
AndyPark20 <apark1214@gmail.com>
2022-10-07 14:19:16 -0700

    working on ruby bank project

---
+++ bank.rb
@@ -0,0 +1,12 @@
+=begin
+Bank Project
+Functions
+User can add relatives to the bank account (Have their own separate account, but tied to the main account)
+User can type their last name and last 4 digits of their social security access their account
+User can withdrawal money from account
+User can add money to the account
+User can view their balance
+User can transfer money to other relatives
+Primary user can ONLY see who has the most and least money
+Primary user can delete relatives from the account
+=end
