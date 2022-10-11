311d5e20fa7ae4a5c650d08d1eb2ea7fdb80d248
AndyPark20 <apark1214@gmail.com>
2022-10-10 19:46:54 -0700

    working on using .each for refractoring

--- refactoring.rb
+++ refactoring.rb
@@ -1,29 +1,36 @@
 
-puts "Its True!" if true
+# puts "Its True!" if true
 
-=begin
-    Same rule applies for Unless
-=end
+# =begin
+#     Same rule applies for Unless
+# =end
 
-puts "Hello World" unless true
+# puts "Hello World" unless true
 
-puts 3<4 ? "Hello" : "Goodbyere"
+# puts 3<4 ? "Hello" : "Goodbyere"
 
 
-##### Working on Case Statement
-puts "Hello there!"
-greeting = gets.chomp
+# ##### Working on Case Statement
+# puts "Hello there!"
+# greeting = gets.chomp
 
-# Add your case statement below!
-case greeting
-  when "English"
-    puts "Hello!"
-  when "French"
-    puts "Bonjour!"
-  when "German"
-    puts "Guten Tag!"
-  when "Finnish"
-    puts "Haloo!"
-  else
-    puts "I dont' know that language!"
-end
+# # Add your case statement below!
+# case greeting
+#   when "English"
+#     puts "Hello!"
+#   when "French"
+#     puts "Bonjour!"
+#   when "German"
+#     puts "Guten Tag!"
+#   when "Finnish"
+#     puts "Haloo!"
+#   else
+#     puts "I dont' know that language!"
+# end
+
+
+
+my_array=[1,2,34,4,5,6]
+
+
+my_array.each {|x| puts x % 2 ==0 ? x : "Not an even number!"}
