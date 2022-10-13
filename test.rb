5b37210a19701e3a92fb56f4ea716ed03d65dfd9
AndyPark20 <apark1214@gmail.com>
2022-10-13 08:27:58 -0700

    learning about symbols and procs

--- test.rb
+++ test.rb
@@ -83,18 +83,25 @@
 # or taller to ride the roller coaster. Let's use .select on
 # each group to get only the ones four feet tall or taller.
 
-group_1 = [4.1, 5.5, 3.2, 3.3, 6.1, 3.9, 4.7]
-group_2 = [7.0, 3.8, 6.2, 6.1, 4.4, 4.9, 3.0]
-group_3 = [5.5, 5.1, 3.9, 4.3, 4.9, 3.2, 3.2]
+# group_1 = [4.1, 5.5, 3.2, 3.3, 6.1, 3.9, 4.7]
+# group_2 = [7.0, 3.8, 6.2, 6.1, 4.4, 4.9, 3.0]
+# group_3 = [5.5, 5.1, 3.9, 4.3, 4.9, 3.2, 3.2]
 
-# Complete this as a new Proc
-over_4_feet = Proc.new {|x| x >=4}
+# # Complete this as a new Proc
+# over_4_feet = Proc.new {|x| x >=4}
 
-# Change these three so that they use your new over_4_feet Proc
-can_ride_1 = group_1.select!(&over_4_feet)
-can_ride_2 = group_2.select!(&over_4_feet)
-can_ride_3 = group_3.select!(&over_4_feet)
+# # Change these three so that they use your new over_4_feet Proc
+# can_ride_1 = group_1.select!(&over_4_feet)
+# can_ride_2 = group_2.select!(&over_4_feet)
+# can_ride_3 = group_3.select!(&over_4_feet)
 
-puts can_ride_1
-puts can_ride_2
-puts can_ride_3
+# puts can_ride_1
+# puts can_ride_2
+# puts can_ride_3
+
+
+numbers_array=[1,2,3,4,5]
+
+to_something = Proc.new{|x| x.to_s}
+
+puts strings_array=numbers_array.map(&to_something)
