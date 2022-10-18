5203c0ca13ec04cbf85fb68d747a3e61da51c76d
AndyPark20 <apark1214@gmail.com>
2022-10-18 11:20:20 -0700

    setting up class

--- class.rb
+++ class.rb
@@ -1,33 +1,6 @@
+class Computer
+    def intialize
 
-class Chef 
-    @@name ="andy"
-    def chef_name
-        puts @@name
     end
 
-    def asian_food
-        puts "I can make Korean food!"
-    end
-
-    def american_food
-        puts "I can make Hamburger and fries!"
-    end
-
-    def italian_food
-        puts "I can make spaghetti!"
-    end
 end
-
-
-class New_chef < Chef
-    def italian_food
-        puts "I can make Pizza!"
-        super
-    end
-end
-
-
-
-another_chef = New_chef.new()
-
-another_chef.chef_name
