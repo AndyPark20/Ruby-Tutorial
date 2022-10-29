94425de685c5f31b210beb6f7e6efdc3225e9d6f
AndyPark20 <apark1214@gmail.com>
2022-10-29 16:11:58 -0700

    find method working

--- find.rb
+++ find.rb
@@ -7,7 +7,7 @@ def matching(user_name, user_ssn, user_password)
         {name: "Jason", ssn: '8989', password: 'good123'}
     ]
 
-    info.find {|x| x[:name] ==user_name && x[:ssn]== user_ssn && x[:password] == user_password}
+   puts info.find {|x| x[:name] ==user_name && x[:ssn]== user_ssn && x[:password] == user_password} ? 'hello!' : "Not found"
 end 
 
-matching('andy', '6789', 'hello123')
+matching('andy', '6789', 'hello1234')
