bb312fa04a5e84e0c9fd3aa8c8ff2d7c035e9365
AndyPark20 <apark1214@gmail.com>
2022-10-29 16:07:42 -0700

    calling method and passing arguments to test

--- find.rb
+++ find.rb
@@ -1,9 +1,13 @@
 
 
 
-def matching
+def matching(user_name, user_ssn, user_password)
     info = [
-        {name: "andy", ssn: '6789', password: 'hello123'}
+        {name: "andy", ssn: '6789', password: 'hello123'},
         {name: "Jason", ssn: '8989', password: 'good123'}
     ]
+
+    info.find {|x| x[:name] ==user_name && x[:ssn]== user_ssn && x[:password] == user_password}
 end 
+
+matching('andy', '6789', 'hello123')
