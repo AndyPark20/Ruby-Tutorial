4ce4a826a5a095f0bf737b9fb3dcda585a90697b
AndyPark20 <apark1214@gmail.com>
2022-10-29 16:27:47 -0700

    in progress

--- find.rb
+++ find.rb
@@ -1,13 +1,15 @@
 
 
 
-def matching(user_name, user_ssn, user_password)
+def check_credentials(user_name, user_ssn, user_password)
     info = [
         {name: "andy", ssn: '6789', password: 'hello123'},
         {name: "Jason", ssn: '8989', password: 'good123'}
     ]
 
-   puts info.find {|x| x[:name] ==user_name && x[:ssn]== user_ssn && x[:password] == user_password} ? 'hello!' : "Not found"
+   info.find do |x| 
+    puts x[:name] ==user_name && x[:ssn]== user_ssn && x[:password] == user_password ? "Hello" : "goodbye"
+   end
 end 
 
-matching('andy', '6789', 'hello1234')
+check_credentials('andy', '6789', 'hello123')
