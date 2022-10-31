a5b8db6abea61591be32e09e43546c1341ec0724
AndyPark20 <apark1214@gmail.com>
2022-10-31 14:14:10 -0700

    learning about re-cursion

--- find.rb
+++ find.rb
@@ -1,24 +1,32 @@
 
 
 
-def check_credentials(user_name, user_ssn, user_password)
-    info = [
-        {name: "andy", ssn: '6789', password: 'hello123'},
-        {name: "Jason", ssn: '8989', password: 'good123'}
-    ]
+# def check_credentials(user_name, user_ssn, user_password)
+#     info = [
+#         {name: "andy", ssn: '6789', password: 'hello123'},
+#         {name: "Jason", ssn: '8989', password: 'good123'}
+#     ]
 
-    # info.find do |users|
-    #     puts users[:name] == user_name && users[:ssn] == user_ssn && users[:password] == user_password 
-    # end
 
-    user_found = info.find {|users| users[:name] == user_name && users[:ssn] == user_ssn && users[:password] == user_password } 
+#     user_found = info.find {|users| users[:name] == user_name && users[:ssn] == user_ssn && users[:password] == user_password } 
 
-    puts user_found ? ("Welcome back #{user_found[:name]}" : "Access denied! #{access_message}"
+#     if user_found 
+#         puts "Welcome back #{user_found[:name]}!"
+#     else
+#         puts "Access Denied!"
+#         access_message
+#     end
+
+#     # puts user_found ? ("Welcome back #{user_found[:name]}" : "Access denied! #{access_message}"
     
-end 
+# end 
 
-def access_message
-    puts "Please re-enter your credentials"
-end
+# def access_message
+#     puts "Please re-enter your credentials"
+# end
 
-check_credentials('andy', '6789', 'hello123s')
+# check_credentials('andy', '6789', 'hello123')
+
+
+# puts "Denied! #{sleep(5)}"
+
