8e8da5abb58bec43382414af59a630300d93f0ff
AndyPark20 <apark1214@gmail.com>
2022-10-31 09:20:38 -0700

    troubleshooting string and method combination in ternary operator

--- find.rb
+++ find.rb
@@ -7,9 +7,14 @@ def check_credentials(user_name, user_ssn, user_password)
         {name: "Jason", ssn: '8989', password: 'good123'}
     ]
 
-   info.find do |x| 
-    puts x[:name] ==user_name && x[:ssn]== user_ssn && x[:password] == user_password ? "Hello" : "goodbye"
-   end
+    # info.find do |users|
+    #     puts users[:name] == user_name && users[:ssn] == user_ssn && users[:password] == user_password 
+    # end
+
+    user_found = info.find {|users| users[:name] == user_name && users[:ssn] == user_ssn && users[:password] == user_password } 
+
+    puts user_found ? "Welcome back #{user_found[:name]}" : "Access denied!"
+    
 end 
 
 check_credentials('andy', '6789', 'hello123')
