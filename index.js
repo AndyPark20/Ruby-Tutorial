e2ab423fef061b66e8319375beb83cae1219ef44
AndyPark20 <apark1214@gmail.com>
2022-10-31 14:51:06 -0700

    setting up functions

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('hello')
+
+
+const users =[
+    {name:'andy', ssn:5678, password:'hello123'},
+    {name:'Jared', ssn:7899, password:'goodbye123'}
+]
+
+const test =()=>{
+    const userFound = users.map((values,index)=>{
+        if(values.name)
+    })
+}
+
+
+test('andy', 5678, 'hello123')
