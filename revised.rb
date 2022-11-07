8ffa44b727916b8a54a89bb01ce7284742676fa0
AndyPark20 <apark1214@gmail.com>
2022-11-07 12:08:40 -0800

    in prpgress

--- revised.rb
+++ revised.rb
@@ -3,41 +3,7 @@
  #This feature will allow user to either sign up for an account.
  #Or log in with their last name,last 4 digits of their social security number, and password.        
 
- module BankFunction
 
-    def self.prompt_commands
-        puts "What would you like to do:"
-        puts " - Deposit"
-        puts " - Withdrawal"
-        puts " - Transfer"
-        print "Please enter from options above:"
-    end
-
-    #method to validate user option command
-    def self.validate_options(user_input_command)
-            case user_input_command
-            when "deposit"
-                return true
-            else
-                prompt_commands
-            end
-    end
-
-    def self.deposit_function(user_accounts, ssn)
-        puts "Ok you will like to deposit"
-        print "Please enter how much you will like to deposit:"
-        user_deposit_input = gets.chomp
-        
-        #Find the user's account and add the deposit amount to their balance
-        user_accounts.each do |users|
-            if users[:ssn][-4..-1] == ssn
-                users[:balance] = user_deposit_input
-            end
-        end
-        print "User Balance #{user_accounts}"
-    end
-
- end
 
  #Create a class that manages all Accounts
 
@@ -53,76 +19,32 @@
 
 
 
-class BankFunction
-
-end
-
 
 class MasterAccount
-
+    @USER_INFO =[
+        {first: 'andy', last: 'park', password: 'hello123', ssn: '123456789', balance:20},
+        {first: 'Jason', last: 'kim', password: 'bye123', ssn: '135791098', balance:10}, 
+    ]
 
 end
 
 class UserAccount
 
-
-end
-
-#class for log in and sign up method
-class Account
-
-    #insert Module BankFunction into class
-    include BankFunction
-
-    USER_INFO=[
-        {first: 'andy', last: 'park', password: 'hello123', ssn: '123456789', balance:20},
-        {first: 'Jason', last: 'kim', password: 'bye123', ssn: '135791098', balance:10},
-    ]
-
-  #If attribute doesn't need to be edited after being initialized, only use reader
-    attr_reader :last_name, :password, :ssn
-
-    def initialize(first_name,last_name, password, ssn)
+    def initialize(first_name, last_name, password, ssn)
         @first_name = first_name
         @last_name = last_name
-        @password = password
         @ssn = ssn
     end
-
-    #Method to check if the users credential exist in the USER_INFO array of objects
-    def validate_log_in_credentials
-        user_found = USER_INFO.find {|user| user[:last].to_s == @last_name && user[:password] == @password && user[:ssn][-4..-1] == @ssn}
-        if user_found 
-            puts "Welcome back #{user_found[:first]} #{user_found[:last]}!"
-            BankFunction.prompt_commands
-            user_function_decision = gets.chomp.downcase
-
-            if BankFunction.validate_options(user_function_decision)
-                BankFunction.deposit_function(USER_INFO, @ssn)
-            end
-            
-        else
-          puts "Access Denied!"
-          new_or_existing_user('y')
-        end
-    end
-
-    #Method to push new user information into USER_INFO array of objects
-    def add_new_user
-        USER_INFO.push({first: @first_name, last: @last_name, password: @password, ssn: @ssn})
-        print "Welcome #{@first_name} #{@last_name} to Bank of Invoca!"
-    end
-
-
 end
-
 
 #############################################
 ######### Main program starts here!##########
 #############################################
+
 puts "Welcome to Bank of Invoca!"
 print "Are you an existing user?(Y/N):"
 existing_new_user = gets.chomp.downcase
+
 
 def new_or_existing_user(user_response)
     case user_response
@@ -168,7 +90,4 @@ def new_or_existing_user(user_response)
 end 
 
 
-
-
 new_or_existing_user(existing_new_user) 
-
