ee88d2e83d638a0d07dbab30943f68184291d973
AndyPark20 <apark1214@gmail.com>
2022-11-07 14:08:59 -0800

    adding new classes to separate functionality

--- revised.rb
+++ revised.rb
@@ -18,22 +18,22 @@
  # 7. Close accounts
 
 
+ class SystemAdmin
+    def add_new_user(first_name, last_name, password, ssn)
+        @user_info_list.push({first: @first_name, last: @last_name, password: @password, ssn: @ssn})
+    end
+ end
 
 
 class MasterAccount
 
-
     def initialize
-        @user_info = [
+        @user_info_list = [
             {first: 'andy', last: 'park', password: 'hello123', ssn: '123456789', balance:20},
             {first: 'Jason', last: 'kim', password: 'bye123', ssn: '135791098', balance:10}, 
         ]
     end
 
-    def render_master_account
-        puts "Currently #{@user_info}"
-    end
-    
 end
 
 class UserAccount
@@ -49,53 +49,53 @@ end
 ######### Main program starts here!##########
 #############################################
 
-# puts "Welcome to Bank of Invoca!"
-# print "Are you an existing user?(Y/N):"
-# existing_new_user = gets.chomp.downcase
+puts "Welcome to Bank of Invoca!"
+print "Are you an existing user?(Y/N):"
+existing_new_user = gets.chomp.downcase
 
 
-# def new_or_existing_user(user_response)
-#     case user_response
-#     when 'y'
-#         #Ask for credentials that will be used to validate sign in
+def new_or_existing_user(user_response)
+    case user_response
+    when 'y'
+        #Ask for credentials that will be used to validate sign in
 
-#         print "Please enter your last name:"
-#         user_last_name_input = gets.chomp.downcase
+        print "Please enter your last name:"
+        user_last_name_input = gets.chomp.downcase
 
-#         print "Please enter your password:"
-#         user_password_input = gets.chomp.downcase
+        print "Please enter your password:"
+        user_password_input = gets.chomp.downcase
 
-#         print "Please enter the last 4 digits of your SSN:"
-#         user_ssn_input = gets.chomp.downcase
+        print "Please enter the last 4 digits of your SSN:"
+        user_ssn_input = gets.chomp.downcase
 
-#         instance_for_log = Account.new(nil,user_last_name_input, user_password_input, user_ssn_input)
-#         instance_for_log.validate_log_in_credentials
-#     when 'n'
-#         print "Please enter your first name:"
-#         user_first_name_input = gets.chomp
-#         print "Please enter your last name:"
-#         user_last_name_input = gets.chomp
-#         print "Please enter your password:"
-#         user_password_input = gets.chomp
-#         print "Please enter your 8 digit SSN number:"
-#         user_ssn_input = gets.chomp
+        instance_for_log = Account.new(nil,user_last_name_input, user_password_input, user_ssn_input)
+        instance_for_log.validate_log_in_credentials
+    when 'n'
+        print "Please enter your first name:"
+        user_first_name_input = gets.chomp
+        print "Please enter your last name:"
+        user_last_name_input = gets.chomp
+        print "Please enter your password:"
+        user_password_input = gets.chomp
+        print "Please enter your 8 digit SSN number:"
+        user_ssn_input = gets.chomp
 
-#         ##Check to see if user_ssn_input is 8 digits
-#         while user_ssn_input.length.to_i <8
-#             print "Please re-enter your 8 digit SSN:"
-#             user_ssn_input= gets.chomp
-#         end 
+        ##Check to see if user_ssn_input is 8 digits
+        while user_ssn_input.length.to_i <8
+            print "Please re-enter your 8 digit SSN:"
+            user_ssn_input= gets.chomp
+        end 
 
-#         ##If user_ssn_input is 8 digits, call add_new_user method from Account instance
-#         instance_new_user= Account.new(user_first_name_input, user_last_name_input, user_password_input, user_ssn_input)
-#         instance_new_user.add_new_user
+        ##If user_ssn_input is 8 digits, call add_new_user method from Account instance
+        instance_new_user= Account.new(user_first_name_input, user_last_name_input, user_password_input, user_ssn_input)
+        instance_new_user.add_new_user
 
-#     else
-#         print "Please re-enter Y or N:"
-#         $existing_new_user = gets.chomp.downcase
-#         new_or_existing_user($existing_new_user)
-#     end
-# end 
+    else
+        print "Please re-enter Y or N:"
+        $existing_new_user = gets.chomp.downcase
+        new_or_existing_user($existing_new_user)
+    end
+end 
 
 
 
