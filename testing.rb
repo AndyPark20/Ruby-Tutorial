1125b8c5f37356521bcb8183b7aa291d9a696db2
AndyPark20 <apark1214@gmail.com>
2022-11-28 15:00:41 -0800

    revising find method:

--- testing.rb
+++ testing.rb
@@ -1,15 +1,20 @@
 print "Hello what is your name?"
 user_info = gets.chomp
 
-r
 
-$master_file = ["andy", "jason"]
+master_file = ["andy", "jason"]
 
-def find_user
-  $master_file.find do |user|
-    if users == user_info
+def find_user(user_list, customer)
+  user_list.find do |user|
+    if user == customer
       print "welcome #{user}"
+      break
+    else
+      print "Sorry we couldn't find that user"
+      break
+    end
   end
 end
 
-find_user
+find_user(master_file, user_info)
+
