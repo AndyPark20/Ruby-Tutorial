7a429cca099c81bc7d601f322bfe3a46c003f6ae
AndyPark20 <apark1214@gmail.com>
2022-11-29 17:07:11 -0800

    adding bank functionality

--- bank-draft.rb
+++ bank-draft.rb
@@ -1,118 +1,98 @@
-=begin
- Feature 1: 
- This feature will allow user to either sign up for an account.
- Or log in with their last name,last 4 digits of their social security number, and password.        
-=end
+# Feature 1:
+# This feature will allow user to either sign up for an account.
+# Or log in with their last name,last 4 digits of their social security number, and password.        
+# class for log in and sign up method
+class Account
+  USER_INFO =
+      [{first: 'andy', last: 'park', password: 'hello123', ssn: '123456789'}, {first: 'Jason', last: 'kim', password: 'bye123', ssn: '135791098'}]
+  
+# If attribute doesn't need to be edited after being initialized, only use reader
+  attr_reader :last_name, :password, :ssn
+  
+      def initialize(first_name,last_name, password, ssn)
+          @first_name = first_name
+          @last_name = last_name
+          @password = password
+          @ssn = ssn
+      end
+  
+      #Method to check if the users credential exist in the USER_INFO array of objects
+      def validate_log_in_credentials
+          user_found = USER_INFO.find {|user| user[:last].to_s == @last_name && user[:password] == @password && user[:ssn][-4..-1] == @ssn}
+          if user_found 
+              puts "Welcome back #{user_found[:first]} #{user_found[:last]}!"
+          else
+            puts "Access Denied!"
+            new_or_existing_user('y')
+          end
+      end
+  
+      #Method to push new user information into USER_INFO array of objects
+      def add_new_user
+          USER_INFO.push({first: @first_name, last: @last_name, password: @password, ssn: @ssn})
+          print "Welcome #{@first_name} #{@last_name} to Bank of Invoca!"
+      end
+  end
+  
+  
+  #############################################
+  ######### Main program starts here!##########
+  #############################################
+  puts "Welcome to Bank of Invoca!"
+  print "Are you an existing user?(Y/N):"
+  existing_new_user = gets.chomp.downcase
+  
+  
+  # Method to determine if the user is new or existing customer
+  def new_or_existing_user(user_response)
+      case user_response
+      when 'y'
+          #Ask for credentials that will be used to validate sign in
+  
+          print "Please enter your last name:"
+          user_last_name_input = gets.chomp.downcase
+  
+          print "Please enter your password:"
+          user_password_input = gets.chomp.downcase
+  
+          print "Please enter the last 4 digits of your SSN:"
+          user_ssn_input = gets.chomp.downcase
+  
+          instance_for_log = Account.new(nil,user_last_name_input, user_password_input, user_ssn_input)
+          instance_for_log.validate_log_in_credentials
+      when 'n'
+          print "Please enter your first name:"
+          user_first_name_input = gets.chomp
+          print "Please enter your last name:"
+          user_last_name_input = gets.chomp
+          print "Please enter your password:"
+          user_password_input = gets.chomp
+          print "Please enter your 8 digit SSN number:"
+          user_ssn_input = gets.chomp
+  
+          ##Check to see if user_ssn_input is 8 digits
+          while user_ssn_input.length.to_i <8
+              print "Please re-enter your 8 digit SSN:"
+              user_ssn_input= gets.chomp
+          end 
+  
+          ##If user_ssn_input is 8 digits, call add_new_user method from Account instance
+          instance_new_user= Account.new(user_first_name_input, user_last_name_input, user_password_input, user_ssn_input)
+          instance_new_user.add_new_user
+  
+      else
+          print "Please re-enter Y or N:"
+          $existing_new_user = gets.chomp.downcase
+          new_or_existing_user($existing_new_user)
+      end
+  end
 
 
-
-#class for log in and sign up method
-class ParentBank
-
-    @@user_info=[
-        {first:'andy', last:'park', password:'hello123', ssn:'123456789'},
-        {first:'Jason', last:'kim', password:'bye123', ssn:'135791098'}
-    ]
-
-    #Instance variable to determine if the user's credential is validated
-    @user_credential_validation = false
-    
-    attr_accessor :last_name, :password, :ssn
-
-    def initialize(first_name,last_name, password, ssn)
-        @first_name = first_name
-        @last_name = last_name
-        @password=password
-        @ssn=ssn
-    end
-
-    def validate_log_in_credentials
-        @@user_info.each do |x|
-            if x[:last].to_s == @last_name && x[:password] == @password && x[:ssn][5,8] == @ssn
-                puts "Welcome back #{x[:first]} #{x[:last]}!"
-                return
-            else
-                puts "sorry access denied!"
-                return
-            end
-        end
-    end
-
-    def create_new_user_account
-        new_user = {first: @first_name, last: @last_name, password: @password, ssn: @ssn}
-        @@user_info.push(new_user)
-      puts @@user_info
-    end
-
-    public
-    def deposit
-
-    end
-
-    def withdrawl
-
-    end
-
-end
-
-
-##########################################################################################
-############################### Main program starts here!#################################
-##########################################################################################
-puts "Welcome to Bank of Invoca!"
-print "Are you an existing user?(Y/N):"
-$existing_new_user = gets.chomp.downcase
-
-
-# Method to determine if the user is new or existing customer
-def new_or_existing_user(user_response)
-   
-    
-    case user_response
-    when 'y'
-        #Ask for credentials that will be used to validate sign in
-        puts "Thank you for being a memeber of Bank of Invoca!!"
-
-        print "Please enter your last name:"
-        user_last_name_input = gets.chomp.downcase
-
-        print "Please enter your password:"
-        user_password_input = gets.chomp.downcase
-
-        print "Please enter the last 4 digits of your SSN:"
-        user_ssn_input = gets.chomp.downcase
-
-        instance_for_log = LogInSignUp.new(nil,user_last_name_input,user_password_input, user_ssn_input)
-        instance_for_log.validate_log_in_credentials
-
-    when 'n'
-        print "Please enter your first name:"
-        user_first_name_input = gets.chomp!
-        print "Please enter your last name:"
-        user_last_name_input = gets.chomp!
-        print "Please enter your password:"
-        user_password_input = gets.chomp!
-        print "Please enter your 8 digit SSN number:"
-        user_ssn_input = gets.chomp!
-    
-
-        ##check to see if user_ssn is 8 digits
-        while user_ssn_input.length.to_i < 8
-            print "Please re-enter your 8 digit SSN:"
-            user_ssn = gets.chomp!
-        end
-    
-        instance_for_log = LogInSignUp.new(user_first_name_input,user_last_name_input,user_password_input, user_ssn_input)
-        instance_for_log.create_new_user_account
-
-       
-        
-    else
-        print "Please re-enter Y or N:"
-        $existing_new_user = gets.chomp.downcase
-        new_or_existing_user($existing_new_user)
-    end
-end
-
-new_or_existing_user($existing_new_user)
-
+  #Bank Functionality
+  def existing_bank_user_functionality
+    print "What would you like to do?"
+  end
+  
+  new_or_existing_user(existing_new_user) 
+  
+  
