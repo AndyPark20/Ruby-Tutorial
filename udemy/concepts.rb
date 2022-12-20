cae5c1bc4cfdbc2879ea139a6a7324cab4b9fe51
AndyPark20 <apark1214@gmail.com>
2022-12-20 14:52:58 -0800

    cannot do string interpolation with single quotes

--- udemy/concepts.rb
+++ udemy/concepts.rb
@@ -1,7 +1,9 @@
+# String concatentation
+sentence = "My name is Andy"
+p sentence
 
+#string interpolation you cannot do with single quotes
+first_name = "Andy"
+last_name = "Smith"
 
-def say_hello(greetings)
-  puts greetings
-end
-
-say_hello "Hello World"
+p "My first name is #{first_name} and my last name is #{last_name}"
