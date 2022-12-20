54c689876566cea886b2af2db844070685c738a4
AndyPark20 <apark1214@gmail.com>
2022-12-20 15:00:06 -0800

    revisiting string methods

--- udemy/concepts.rb
+++ udemy/concepts.rb
@@ -7,3 +7,17 @@ first_name = "Andy"
 last_name = "Smith"
 
 p "My first name is #{first_name} and my last name is #{last_name}"
+
+#Methods, how to find them
+p 10.nil?
+p 10.to_s.class
+p "".nil?
+p "".empty?
+
+#common methods
+
+
+#Variable assignment
+
+
+#Escaping 
