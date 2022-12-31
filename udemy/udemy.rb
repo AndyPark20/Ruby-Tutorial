1dac6a31266cc386c861058f0598e7319f93d2d0
Andy Park <apark1214@gmail.com>
2022-12-31 14:56:39 -0800

    refreshing on ruby basics

--- udemy/udemy.rb
+++ udemy/udemy.rb
@@ -1,10 +1,19 @@
 
 
-print "Please enter your name:"
+print "Please enter a number to play with:"
 user_input = gets.chomp
 
-def greetings (arg1)
-  print "welcome back #{arg1}"
+def integer_value_checker(value)
+  if value.to_i < 3
+    integer_calculator
+  else
+    print "Value is greater than the requirement"
+  end
 end
 
-greetings(user_input)
+
+def integer_calculator
+  print "Method here"
+end
+
+integer_value_checker(user_input)
