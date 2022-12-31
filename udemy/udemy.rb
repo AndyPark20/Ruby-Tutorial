ed41aedf5d68ce82586f761839b38f471990d7f7
Andy Park <apark1214@gmail.com>
2022-12-31 14:30:09 -0800

    working on functions

--- udemy/udemy.rb
+++ udemy/udemy.rb
@@ -1,2 +1,10 @@
-print 10 != "Hello"
-print 10 == 10
+
+
+print "Please enter your name:"
+user_input = gets.chomp
+
+def greetings (arg1)
+  print "welcome back #{arg1}"
+end
+
+greetings(user_input)
