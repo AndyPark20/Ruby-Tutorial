7ce0e7717cc13e5196ace4815be00f4b80960fb2
Andy Park <apark1214@gmail.com>
2023-01-04 15:44:54 -0800

    in progress

--- udemy/authenticator/authenticator.rb
+++ udemy/authenticator/authenticator.rb
@@ -7,12 +7,31 @@ users =[
 
 def initial_prompt
   print "Please press N to exit out or press ENTER to continue:"
-  user_input = gets.chomp
+  user_input = gets.chomp.downcase
+end
+
+def get_credentials
+  print "Please enter username:"
+  user_username = gets.chomp
+  user_password = nil
+
+  if user_username
+    print "Please enter password:"
+    user_password = gets.chomp
+
+    print "You have entered username: #{user_username} and password: #{user_password}"
+  end
 end
 
 
+
 def validate_user_prompt
-  print initial_prompt
+  case initial_prompt
+    when 'n'
+      get_credentials
+    when ''
+      puts 'bye'
+  end
 end
 
 validate_user_prompt
