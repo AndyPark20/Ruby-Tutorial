4576405dadc4a71db95791e8dffdeb552a5879f9
Andy Park <apark1214@gmail.com>
2023-01-05 15:38:17 -0800

    working on styling

--- udemy/authenticator/authenticator.rb
+++ udemy/authenticator/authenticator.rb
@@ -1,13 +1,14 @@
 $USERS =[
-  {username:'andy', password:'password1'},
-  {username:'Lewis', password:'password2'},
-  {username:'Lance', password:'password3'},
-  {username:'Mick', password:'password4'},
+  { username:'andy', password:'password1' },
+  { username:'Lewis', password:'password2' },
+  { username:'Lance', password:'password3' },
+  { username:'Mick', password:'password4' },
 ]
 
 def initial_prompt
   print "Please press N to exit out or press y to continue:"
   user_input = gets.chomp.downcase
+
 end
 
 def get_credentials
@@ -23,9 +24,9 @@ def get_credentials
 end
 
 def validate_credentials(username, password)
-    found_user = $USERS.select {|user_info| user_info[:username] == username && user_info[:password] == password}
+    found_user = $USERS.select { |user_info| user_info[:username] == username && user_info[:password] == password }
     if found_user.length !=0
-      print "Welcome back #{found_user[0][:username]}"
+      print "Welcome back #{ found_user[0][:username] }"
     else
       puts "Sorry credentials invalid"
       get_credentials
