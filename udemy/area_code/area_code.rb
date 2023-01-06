c93db38342572bc9054fd3ab299ee678e079050a
Andy Park <apark1214@gmail.com>
2023-01-05 16:04:07 -0800

    working on prompts

--- udemy/area_code/area_code.rb
+++ udemy/area_code/area_code.rb
@@ -1,8 +1,30 @@
 dial_book = {
   newyork: "212",
   newbrunswick: "732",
-  salinas: "831"
+  salinas: "831",
   irvine: "949",
   sanjose: "408",
   fremont: "510"
 }
+
+
+
+def get_city_names(somehash)
+
+end
+
+def get_area_code(somehash,key)
+
+end
+
+loop do
+  print "Do you want to lookup an area ccode based on a city name?(Y/N):"
+  user_response = gets.chomp.downcase
+  case
+  when user_response != 'y'
+    puts "Have a great day!"
+    break
+  else
+    get_city_names
+  end
+end
