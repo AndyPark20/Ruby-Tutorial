2b1f5cefc900c613b38a340837f7831a795dd2fb
Andy Park <apark1214@gmail.com>
2023-01-06 15:47:18 -0800

    working on initializer

--- udemy/oop/kick_off.rb
+++ udemy/oop/kick_off.rb
@@ -1,14 +1,16 @@
 class Student
-  @first_name
-  @last_name
-  @email
-  @username
-  @password
+  attr_accessor :first_name, :last_name, :email, :username, :password
 
-  #setter
-  def first_name= (name)
-    @first_name = name
+  def initialize(firstname, lastname, username, email, password)
+    @first_name = firstname
+    @last_name = lastname
+    @user_name = username
+    @email = email
+    @password = password
   end
+
+
+
 
   def to_s
     "Hello #{@first_name}"
@@ -16,7 +18,5 @@ class Student
 
 end
 
-andy = Student.new
-
-andy.first_name = "andy"
-puts andy.first_name
+andy = Student.new("andy", "park", "hiandy898989", "testing@gmail.com", "1234949")
+jason = Student.new("andy", "park", "hiandy898989", "testing@gmail.com", "1234949")
