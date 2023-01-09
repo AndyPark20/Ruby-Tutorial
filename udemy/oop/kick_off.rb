b0e62114a82c700e9b5d376bc34140f9e61d0b2c
Andy Park <apark1214@gmail.com>
2023-01-09 08:25:58 -0800

    comparing different salts

--- udemy/oop/kick_off.rb
+++ udemy/oop/kick_off.rb
@@ -56,7 +56,12 @@
 require "bcrypt"
 
 my_password = BCrypt::Password.create("my password")
+my_password_1 = BCrypt::Password.create("my password")
+my_password_2 = BCrypt::Password.create("my password")
 
-puts my_password
-puts my_password.version
 puts my_password == "my password"
+puts my_password_1 == "my password"
+puts my_password_2 == "my password"
+
+# my_password = BCrypt::Password.new("$2a$12$GFzTKQB9UCGP4MV6RTLti.oUWg.qN/CK4tLRBQnjblM3s0MkTriVq")
+# puts my_password == "my password"
